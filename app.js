5f69791973fbf356d2a6016ff3c1f61c55967bbd
AhnKongudom <107701877+AhnKongudom@users.noreply.github.com>
2022-06-18 08:54:18 +0700

    Add all files of Project
    Hello World :100:

---
+++ app.js
@@ -0,0 +1,24 @@
+const express =  require('express');
+const chalk = require('chalk')
+const app = express();
+//const port = 3000; 
+const debug = require ('debug')('app');
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res)=>{
+
+    res.send('Hello borntoDev 005');
+})
+
+app.listen(port, ()=>{
+
+    //console.log("Listening on port " + chalk.red(" : ",port));
+    debug("Listening on port" + chalk.green(port));
+})
+
+
+
+
+
